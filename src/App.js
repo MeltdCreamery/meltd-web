7256dc76b8ec1141eaca5ea201a26601a47683a2
Jenny <jrw3mx@virginia.edu>
2020-08-04 19:32:34 -0400

    router set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>Hello!</div>
+
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact Us</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route path="/contact">
+                        <Contact />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+
     </div>
   );
 }
---
+++ src/views/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Contact = (props) => {
+
+
+
+    return (
+        <div>
+            Contact us!
+        </div>
+    );
+};
+
+
+export default Contact;
