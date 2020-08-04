a8aaf2bcfca89f2d26bc5e846e554bf8fe7697e4
Jenny <jrw3mx@virginia.edu>
2020-08-04 19:44:44 -0400

    config prettier

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import Home from "./views/Home";
+import Contact from "./views/Contact";
+import "./App.css";
 
 const App = () => {
-  return (
-    <div className="App">
-        <div>Hello!</div>
+    return (
+        <div className="App">
+            <div>Hello!</div>
 
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact">Contact Us</Link>
-                        </li>
-                    </ul>
-                </nav>
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact">Contact Us</Link>
+                            </li>
+                        </ul>
+                    </nav>
 
-                <Switch>
-                    <Route path="/contact">
-                        <Contact />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-
-    </div>
-  );
-}
+                    <Switch>
+                        <Route path="/contact">
+                            <Contact />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </div>
+    );
+};
 
 export default App;
